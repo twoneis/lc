fa2aaed4a0759e5ebb9b92b0e2378e18901fa43a
twoneis <sanjay29@sapura.de>
2024-04-14 13:07:46 +0200

    misc: add flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: inputs.utils.lib.eachSystem [
+    "x86_64-linux" "i686-linux" "aarch64-linux" "x86_64-darwin"
+  ] (system: let
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          python3
+        ];
+      };
+    });
+}
